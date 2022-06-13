db399915811136171229361e87bc23c1b347d523
Abdullah <abdullah@pop-os.localdomain>
2022-06-13 22:22:58 +0200

    cleanup

--- vars.tf
+++ vars.tf
@@ -1,4 +1,12 @@
 variable "ami_type" {
-    type = string
-    default = "ami-09439f09c55136ecf" 
+  type    = string
+  default = "ami-09439f09c55136ecf"
+}
+
+variable "access_key" {
+  type = string
+}
+
+variable "secret_key" {
+  type = string
 }
--- vpc.tf
+++ vpc.tf
@@ -1,5 +1,5 @@
 resource "aws_instance" "helloworld" {
-  ami = var.ami_type
+  ami           = var.ami_type
   instance_type = "t2.micro"
   tags = {
     Name = "HelloWorld"
